264dd4782562151cb447c4e29d88a98e1988ccc5
Reuben Cummings <reubano@gmail.com>
2020-04-25 02:22:19 -0500

    Switch theme

--- docs/conf.py
+++ docs/conf.py
@@ -50,22 +50,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-html_theme_options = {
-    # 'logo': 'logo.png',
-    # 'touch_icon': 'alabaster',
-    'description': 'Everything you need to excel at Nerevu',
-    'fixed_sidebar': True,
-    'sidebar_width': '200px',
-    'show_relbar_bottom': True,
-    'show_related': True,
-    'github_user': 'nerevu',
-    'github_repo': 'handbook',
-}
